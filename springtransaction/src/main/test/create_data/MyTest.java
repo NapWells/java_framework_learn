8fb16485a5850f40128564222ba6a79bd852bb54
lhy <hy.liu@fentuan360.com>
2019-01-31 12:49:43 +0800

    'testdata'

---
+++ springtransaction/src/main/test/create_data/MyTest.java
@@ -0,0 +1,208 @@
+package create_data;
+
+import java.io.UnsupportedEncodingException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MyTest {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        final String url = "jdbc:mysql://localhost:3306/test?characterEncoding=UTF-8&useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+        final String name = "com.mysql.cj.jdbc.Driver";
+        final String user = "root";
+        final String password = "123456";
+        Connection conn = null;
+        Class z =  Class.forName(name);//指定连接类型
+        conn = DriverManager.getConnection(url, user, password);//获取连接
+        if (conn!=null) {
+            System.out.println("获取连接成功");
+            insert(conn);
+        }else {
+            System.out.println("获取连接失败");
+        }
+    }
+
+    public static void insert(Connection conn) {
+        // 开始时间
+        Long begin = new Date().getTime();
+        // sql前缀
+
+
+        String prefix = "INSERT INTO user_info (`phone`,`name`,`eduction`,`age`,`sex`,`id_no`) VALUES ";
+        String sql = null;
+        try {
+            // 保存sql后缀
+            StringBuffer suffix = new StringBuffer();
+            // 设置事务为非自动提交
+            conn.setAutoCommit(false);
+            // 比起st，pst会更好些
+            // 外层循环，总提交事务次数
+            PreparedStatement pst = null;
+            String value = "('%s','%s','%s',%d,'%s','%s'),";
+            IdCardGenerator g = new IdCardGenerator();
+            for (int i = 1; i <= 100; i++) {
+                // 第j次提交步长
+                for (int j = 1; j <= 50000; j++) {
+                    // 构建SQL后缀
+                    String phone = getTel();
+                    String name = getName();
+                    String eduction = getEduction();
+                    String sex = random.nextBoolean() ? "男" : "女";
+                    String id_no = g.generate();
+                    int age = g.getAgeByCertId(id_no);
+                    suffix.append(String.format(value,phone,name,eduction,age,sex,id_no));
+                }
+                // 构建完整SQL
+                sql = prefix + suffix.substring(0, suffix.length() - 1);
+                pst = (PreparedStatement) conn.prepareStatement(sql);//准备执行语句
+                // 添加执行SQL
+                pst.addBatch(sql);
+                // 执行操作
+                pst.executeBatch();
+                // 提交事务
+                conn.commit();
+                // 清空上一次添加的数据
+                suffix = new StringBuffer();
+            }
+            // 头等连接
+            pst.close();
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println(sql);
+            e.printStackTrace();
+        }
+        // 结束时间
+        Long end = new Date().getTime();
+        // 耗时
+        System.out.println("1000万条数据插入花费时间 : " + (end - begin) / 1000 + " s");
+        System.out.println("插入完成");
+    }
+
+    /**
+     * 返回手机号码
+     */
+    private static String[] telFirst="134,135,136,137,138,139,150,151,152,157,158,159,130,131,132,155,156,133,153".split(",");
+    private static String getTel() {
+        int index=getNum(0,telFirst.length-1);
+        String first=telFirst[index];
+        String second=String.valueOf(getNum(1,888)+10000).substring(1);
+        String third=String.valueOf(getNum(1,9100)+10000).substring(1);
+        return first+second+third;
+    }
+    public static int getNum(int start,int end) {
+        return (int)(Math.random()*(end-start+1)+start);
+    }
+
+    private final  static Random random = new Random();
+
+    private static String[] firstNames= {"赵","钱","孙","李","周","吴","郑","王","冯","陈","褚","卫","蒋","沈","韩","杨","朱","秦","尤","许",
+            "何","吕","施","张","孔","曹","严","华","金","魏","陶","姜","戚","谢","邹","喻","柏","水","窦","章","云","苏","潘","葛","奚","范","彭","郎",
+            "鲁","韦","昌","马","苗","凤","花","方","俞","任","袁","柳","酆","鲍","史","唐","费","廉","岑","薛","雷","贺","倪","汤","滕","殷",
+            "罗","毕","郝","邬","安","常","乐","于","时","傅","皮","卞","齐","康","伍","余","元","卜","顾","孟","平","黄","和",
+            "穆","萧","尹","姚","邵","湛","汪","祁","毛","禹","狄","米","贝","明","臧","计","伏","成","戴","谈","宋","茅","庞","熊","纪","舒",
+            "屈","项","祝","董","梁","杜","阮","蓝","闵","席","季","麻","强","贾","路","娄","危","江","童","颜","郭","梅","盛","林","刁","钟",
+            "徐","邱","骆","高","夏","蔡","田","樊","胡","凌","霍","虞","万","支","柯","昝","管","卢","莫","经","房","裘","缪","干","解","应",
+            "宗","丁","宣","贲","邓","郁","单","杭","洪","包","诸","左","石","崔","吉","钮","龚","程","嵇","邢","滑","裴","陆","荣","翁","荀",
+            "羊","于","惠","甄","曲","家","封","芮","羿","储","靳","汲","邴","糜","松","井","段","富","巫","乌","焦","巴","弓","牧","隗","山",
+            "谷","车","侯","宓","蓬","全","郗","班","仰","秋","仲","伊","宫","宁","仇","栾","暴","甘","钭","厉","戎","祖","武","符","刘","景",
+            "詹","束","龙","叶","幸","司","韶","郜","黎","蓟","溥","印","宿","白","怀","蒲","邰","从","鄂","索","咸","籍","赖","卓","蔺","屠",
+            "蒙","池","乔","阴","郁","胥","能","苍","双","闻","莘","党","翟","谭","贡","劳","逄","姬","申","扶","堵","冉","宰","郦","雍","却",
+            "璩","桑","桂","濮","牛","寿","通","边","扈","燕","冀","浦","尚","农","温","别","庄","晏","柴","瞿","阎","充","慕","连","茹","习",
+            "宦","艾","鱼","容","向","古","易","慎","戈","廖","庾","终","暨","居","衡","步","都","耿","满","弘","匡","国","文","寇","广","禄",
+            "阙","东","欧","殳","沃","利","蔚","越","夔","隆","师","巩","厍","聂","晁","勾","敖","融","冷","訾","辛","阚","那","简","饶","空",
+            "曾","毋","沙","乜","养","鞠","须","丰","巢","关","蒯","相","查","后","荆","红","游","郏","竺","权","逯","盖","益","桓","公","仉",
+            "督","岳","帅","缑","亢","况","郈","有","琴","归","海","晋","楚","闫","法","汝","鄢","涂","钦","商","牟","佘","佴","伯","赏","墨",
+            "哈","谯","篁","年","爱","阳","佟","言","福","南","火","铁","迟","漆","官","冼","真","展","繁","檀","祭","密","敬","揭","舜","楼",
+            "疏","冒","浑","挚","胶","随","高","皋","原","种","练","弥","仓","眭","蹇","覃","阿","门","恽","来","綦","召","仪","风","介","巨",
+            "木","京","狐","郇","虎","枚","抗","达","杞","苌","折","麦","庆","过","竹","端","鲜","皇","亓","老","是","秘","畅","邝","还","宾",
+            "闾","辜","纵","侴","万俟","司马","上官","欧阳","夏侯","诸葛","闻人","东方","赫连","皇甫","羊舌","尉迟","公羊","澹台","公冶","宗正",
+            "濮阳","淳于","单于","太叔","申屠","公孙","仲孙","轩辕","令狐","钟离","宇文","长孙","慕容","鲜于","闾丘","司徒","司空","兀官","司寇",
+            "南门","呼延","子车","颛孙","端木","巫马","公西","漆雕","车正","壤驷","公良","拓跋","夹谷","宰父","谷梁","段干","百里","东郭","微生",
+            "梁丘","左丘","东门","西门","南宫","第五","公仪","公乘","太史","仲长","叔孙","屈突","尔朱","东乡","相里","胡母","司城","张廖","雍门",
+            "毋丘","贺兰","綦毋","屋庐","独孤","南郭","北宫","王孙"};
+
+
+    public static String getName(){
+        int length = 1 + random.nextInt(2);
+        StringBuilder sb = new StringBuilder();
+        sb.append(firstNames[random.nextInt(firstNames.length)]);
+        for (int i = 0 ; i < length ; i++){
+           sb.append(getChinese());
+        }
+        return  sb.toString() ;
+    }
+
+    public static String getChinese() {
+        String str = null;
+        int highPos, lowPos;
+        Random random = new Random();
+        highPos = (176 + Math.abs(random.nextInt(71)));//区码，0xA0打头，从第16区开始，即0xB0=11*16=176,16~55一级汉字，56~87二级汉字
+        random = new Random();
+        lowPos = 161 + Math.abs(random.nextInt(94));//位码，0xA0打头，范围第1~94列
+
+        byte[] bArr = new byte[2];
+        bArr[0] = (new Integer(highPos)).byteValue();
+        bArr[1] = (new Integer(lowPos)).byteValue();
+        try {
+            str = new String(bArr, "GB2312");    //区位码组合成汉字
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+
+
+
+    private static String []eductions = "小学|初级中学|高级中学|大学专科|大学本科|硕士研究生|博士研究生|其他".split("\\|");
+    public static String getEduction(){
+        return eductions[random.nextInt(eductions.length-1)];
+    }
+
+    public static String stringToUnicode(String s) {
+        try {
+            StringBuffer out = new StringBuffer("");
+            //直接获取字符串的unicode二进制
+            byte[] bytes = s.getBytes("unicode");
+            //然后将其byte转换成对应的16进制表示即可
+            for (int i = 0; i < bytes.length - 1; i += 2) {
+                out.append("\\u");
+                String str = Integer.toHexString(bytes[i + 1] & 0xff);
+                for (int j = str.length(); j < 2; j++) {
+                    out.append("0");
+                }
+                String str1 = Integer.toHexString(bytes[i] & 0xff);
+                out.append(str1);
+                out.append(str);
+            }
+            return out.toString();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public static String unicodeToString(String str) {
+
+        Pattern pattern = Pattern.compile("(\\\\u(\\p{XDigit}{4}))");
+        Matcher matcher = pattern.matcher(str);
+        char ch;
+        while (matcher.find()) {
+            //group 6728
+            String group = matcher.group(2);
+            //ch:'木' 26408
+            ch = (char) Integer.parseInt(group, 16);
+            //group1 \u6728
+            String group1 = matcher.group(1);
+            str = str.replace(group1, ch + "");
+        }
+        return str;
+
+    }
+}
+
